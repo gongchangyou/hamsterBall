b382b3dc0916096d4a2014d8385f50b96df37489
gongchangyou <gongchangyou@gmail.com>
2015-08-12 11:40:06 +0800

    mod angular drag

--- Assets/Area1.cs
+++ Assets/Area1.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class Area1 : Area {
 	// Use this for initialization
 	void Awake(){
-		maxSeconds = 15.0f;
+		maxSeconds = 25.0f;
 		base.Awake ();
 		sphereStartPos = sphere.transform.position;
 		cameraStartPos = camera.transform.position;// new Vector3 (0,13,-12); 
--- Assets/Sphere.cs
+++ Assets/Sphere.cs
@@ -2,13 +2,26 @@
 using System.Collections;
 
 public class Sphere : MonoBehaviour {
-	
+
+	private float angularDrag = 10.0f;
 	// Use this for initialization
 	void Start () {
+		rigidbody.angularDrag = angularDrag;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		GameObject area = GameObject.Find("Area");
+		if (area.GetComponent<Area> ().canMove){
+		    if(rigidbody.velocity.y < -0.01f) {
+				rigidbody.velocity *= 1.01f;
+				rigidbody.angularDrag = 0.0f;
+			}else{
+				rigidbody.angularDrag = angularDrag;
+			}
+		}
+		Debug.Log ("sphere angurlar" + rigidbody.angularVelocity);
+
 	}
 
 	void OnCollisionEnter(Collision collisionInfo)
